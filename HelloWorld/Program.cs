b2f838b2360676bf266f8cfa4c1c6c0b7cd4e20c
Vladyslav <lgl90pro@gmail.com>
2020-10-31 23:26:16 +0200

    Commit 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace HelloWorld
 {
@@ -6,7 +7,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please, tell me your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Your age: {age}.");
+            Console.ReadKey();
         }
     }
 }
